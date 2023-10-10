79e3dd39a433e579baea24b637cc4d180f6a32a9
Eliot-Shaw <eliot.shaw@proton.me>
2023-10-10 09:24:14 +0200

    creation d'un site v2 pour php

---
+++ SitePro/v2/index.php
@@ -0,0 +1,12 @@
+<?php
+    echo date('d-m-y h:i:s');
+
+
+    echo "<h2>Première instruction PHP avec print</h2>";
+
+    echo 'Je m\'appelle Florian mais tout le monde m\'appelle "Flo"<br>';
+    echo "Je m'appelle Florian mais tout le monde m'appelle \"Flo\"<br>";
+    print 'Je m\'appelle Florian mais tout le monde m\'appelle "Flo"<br>';
+    print "Je m'appelle Florian mais tout le monde m'appelle \"Flo\"<br>";
+?>
+
