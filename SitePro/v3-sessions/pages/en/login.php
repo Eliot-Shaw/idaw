eadbe1e135490fee47b0eb43ba044050e5ddc4bb
Eliot-Shaw <eliot.shaw@proton.me>
2023-10-13 08:54:05 +0200

    connect and disconnect en

--- SitePro/v3-sessions/pages/en/login.php
+++ SitePro/v3-sessions/pages/en/login.php
@@ -1,20 +1,20 @@
-<form id="login_form" action="pages/fr/connected.php" method="POST">
+<form id="login_form" action="pages/en/connected.php" method="POST">
     <table>
         <tr>
             <th>Login :</th>
             <td><input type="text" name="login"></td>
             </tr>
             <tr>
-            <th>Mot de passe :</th>
+            <th>Password :</th>
             <td><input type="password" name="password"></td>
             </tr>
             <tr>
             <th></th>
-            <td><input type="submit" value="Se connecter..." /></td>
+            <td><input type="submit" value="Connect..." /></td>
         </tr>
     </table>
 </form>
 
 <br>
 
-<h3> Deconnexion en cliquant sur le nom d'utilisateur dans le menu </h3>
+<h3> Disconnect by clicking on your name above the menu </h3>
