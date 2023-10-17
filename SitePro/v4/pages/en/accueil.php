364e6d5cb75155d5dfc121870c3d0010115beba6
Eliot-Shaw <eliot.shaw@proton.me>
2023-10-17 08:49:44 +0200

    pas besoin du v4

--- SitePro/v4/pages/en/accueil.php
+++
@@ -1,9 +0,0 @@
-<h1>Welcome to our website</h1>
-<p>We are delighted to welcome you to our website. Here, you will find all the relevant information about our company and our services.</p>
-<p>Feel free to browse through our various pages to learn more about what we offer.</p>
-<p>If you have any questions or need assistance, please don't hesitate to contact us. We are here to help you.</p>
-<p>Thank you for visiting, and we hope you enjoy your experience on our site.</p>
-
-
-<br>
-<h3>If you want to get back on the home page, click the image on the top left corner of the website</h3>
--- SitePro/v4/pages/fr/accueil.php
+++
@@ -1,9 +0,0 @@
-<h1>Bienvenue sur mon site internet</h1>
-<p>Nous sommes heureux de vous accueillir sur notre site web. Ici, vous trouverez toutes les informations pertinentes sur notre entreprise et nos services.</p>
-<p>N'hésitez pas à naviguer à travers nos différentes pages pour en savoir plus sur ce que nous proposons.</p>
-<p>Si vous avez des questions ou avez besoin d'assistance, n'hésitez pas à nous contacter. Nous sommes là pour vous aider.</p>
-<p>Merci de votre visite, et nous espérons que vous apprécierez votre expérience sur notre site.</p>
-
-
-<br>
-<h3>Si vous souhaitez revenir sur cette page, il vous suffit de cliquer sur l'image en haut à gauche du site</h3>
