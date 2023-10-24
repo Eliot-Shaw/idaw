00ecad0668ff6c0f94dfc2e96df1f6edc7d59ac7
Eliot-Shaw <eliot.shaw@proton.me>
2023-10-24 17:17:57 +0200

    ajout d'un draw.io pour faire le MCD et MLD

---
+++ Projet/BDD/config.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+
+<body>
+    <?php
+    define('_MYSQL_HOST', 'localhost');
+    define('_MYSQL_PORT', 3306);
+    define('_MYSQL_DBNAME', 'dbtest');
+    define('_MYSQL_USER', 'root');
+    define('_MYSQL_PASSWORD', '');
+    ?>
+</body>
+
+</html>
