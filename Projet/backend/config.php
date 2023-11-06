a8c56e2b7edc4c765645a3c82647606cff8a847d
Eliot-Shaw <139589770+Eliot-Shaw@users.noreply.github.com>
2023-11-06 09:39:20 +0100

    Delete Projet/backend/config.php

--- Projet/backend/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define('_MYSQL_HOST', 'localhost');
-    define('_MYSQL_PORT', 3306);
-    define('_MYSQL_DBNAME', 'idaw_projet-food_tracker');
-    define('_MYSQL_USER', 'root');
-    define('_MYSQL_PASSWORD', '');
-?>
