80d87224bbd58cc0aebe3ff433b73fcc5cf3e462
Eliot-Shaw <eliot.shaw@proton.me>
2023-11-10 07:25:28 +0100

    fik la bdbd

--- Projet/backend/init_db.php
+++ Projet/backend/init_db.php
@@ -5,9 +5,4 @@
     $request_create = $pdo->prepare($sql_create);
     $request_create->execute();
     $request_create->closeCursor();
-
-    // $sql_fill = file_get_contents('sql/fill_db.sql');
-    // $request_fill = $pdo->prepare($sql_fill);
-    // $request_fill->execute();
-    // $request_fill->closeCursor(); 
 ?>
